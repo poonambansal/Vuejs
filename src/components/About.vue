4031cc80680c8cf11c0165ef137a684efc48e128
Arnav Jain <arnav.jain@dotsquares.com>
2020-10-01 11:12:56 +0530

    first assignment- Changes

---
+++ src/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>My About-Us Page</h1>
+    
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+
---
+++ src/components/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>My Contact Page</h1>
+    
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+
